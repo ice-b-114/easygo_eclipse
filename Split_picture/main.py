47f8db1ad3d2c575fa15ff85b875bbe6395ad79f
浩毅 陈 <chenhaoyi1@outlook.com>
2019-11-26 11:12:17 +0800

    11.26

--- Split_picture/main.py
+++ Split_picture/main.py
@@ -7,7 +7,7 @@ from loguru import logger
 logger.add("output.log", backtrace=True, diagnose=True)
 try:
     filepath = r"C:/Users\JSKJ\Desktop\shenhe"#总文件夹所在的上级目录的路径
-    files = os.listdir(filepath)#
+    files = os.listdir(filepath)
     for name in files:
         Merge_data_set.main(filepath,name)
         data.main(filepath,name)
