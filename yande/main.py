ce383830e9c2522fec2afc1a010e399bcdb68d85
浩毅 陈 <chenhaoyi1@outlook.com>
2019-12-09 18:41:05 +0800

    12.9

--- yande/main.py
+++ yande/main.py
@@ -1,16 +1,39 @@
 # -*- coding: UTF-8 -*-
 import requests
 import re
-
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+#download_list = multiprocessing.Manager().dict()
 def download(img_url,pname):
-    print("Downloading...")
-    r = requests.get(img_url)
+    global download_list
+    sec = time.time()#计时
+    r = requests.get(img_url,stream=True)
+    pic_size = float(r.headers['content-length'])
     #print(r.status_code) # 返回状态码
     if r.status_code == 200:
-        open(pname+'.jpg', 'wb').write(r.content) # 将内容写入图片
-        print("Done\n")
+        f = open(pname+'.jpg', 'wb')
+        #显示文件下载进度
+        count = 0
+        timetemp = time.time()
+        for chunk in r.iter_content(chunk_size = 512):
+            if chunk:
+                f.write(chunk)
+                count += len(chunk)
+                if time.time() - timetemp > 1:
+                    p = count / pic_size * 100
+                    print('{}:{:.2f}%'.format(pname,p))
+                    #download_list[pname] = p
+                    timetemp = time.time()
+        f.close()
+        sec = time.time() - sec
+        res_text = "{} Done,{:.2f}s used".format(pname,sec)
     else:
-        print("Fail")
+        res_text = "{} Fail".format(pname)
+    return res_text
+def showdownloadlist():
+    while True :
+        time.sleep(2)
+        print(str(download_list))
 def main(limit,*tag):
     tags = '+'.join(tag)
     res=requests.get(url='http://yande.re/post.xml?limit='+str(limit)+'&tags='+tags)
@@ -23,8 +46,18 @@ def main(limit,*tag):
         print('ID:'+pic_name[i])
         print('source:'+pic_author[i])
         print('tags:'+pic_tag[i])
-        download(pic_address[i],pic_name[i])
         with open('info.txt', "a+",encoding='utf-8-sig') as f:
             f.write('ID:{}\nsource:{}\ntags:{}\n'.format(pic_name[i],pic_author[i],pic_tag[i]))
+#多线程下载
+    executor = ThreadPoolExecutor(max_workers=5)
+    #all_task = [executor.submit(download,pic_address[i],pic_name[i]) for i in range(0,limit)]
+    all_task = []
+    #executor.submit(showdownloadlist())
+    for i in range(0,limit):
+        all_task.append(executor.submit(download,pic_address[i],pic_name[i]))
+        print("{} Downloading...".format(pic_name[i]))
+    for res in as_completed(all_task):
+        print(res.result())
 if __name__ == '__main__':
-    main(5,"breasts")#order%3Arandom
+    main(2,"bottomless order%3Arandom")#order%3Arandom
+    print("All downloading task done")
